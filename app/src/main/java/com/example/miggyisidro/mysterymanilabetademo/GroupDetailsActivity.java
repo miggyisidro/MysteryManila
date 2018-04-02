bdfd3cb9caa0eb03909ce7f5d8ddb58ded2025f6
veejay1818 <veejay_banda@ymail.com>
2018-04-03 06:25:56 +0800

    Added post game activity and viewpostgame add sa gameinput table using payment activity try mo din sa player details

--- app/src/main/java/com/example/miggyisidro/mysterymanilabetademo/GroupDetailsActivity.java
+++ app/src/main/java/com/example/miggyisidro/mysterymanilabetademo/GroupDetailsActivity.java
@@ -5,10 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class GroupDetailsActivity extends AppCompatActivity {
 
     private Button nextPage;
+    private EditText groupName;
+    private RadioGroup group;
+    private RadioButton radio;
 
 
     @Override
@@ -17,10 +23,17 @@ public class GroupDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_group_details);
 
         nextPage = (Button) findViewById(R.id.button);
+        group = (RadioGroup) findViewById(R.id.radioGroup);
+
 
         nextPage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                int selectedDiscount = group.getCheckedRadioButtonId();
+
+                radio = (RadioButton) findViewById(selectedDiscount);
+
                 Intent next = new Intent(GroupDetailsActivity.this, OCRActivity.class);
                 startActivity(next);
             }
---
+++ app/src/main/java/com/example/miggyisidro/mysterymanilabetademo/ViewGamePostGame.java
@@ -0,0 +1,13 @@
+package com.example.miggyisidro.mysterymanilabetademo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewGamePostGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_game_post_game);
+    }
+}
