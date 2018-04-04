be7bc83f13c25361a327c41d8d69a74ae2ab8e05
MiggyIsidro <miggy.0107@gmail.com>
2018-04-05 00:25:47 +0800

    patest

--- app/src/main/java/com/example/miggyisidro/mysterymanilabetademo/OngoingActivity.java
+++ app/src/main/java/com/example/miggyisidro/mysterymanilabetademo/OngoingActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -31,6 +32,12 @@ public class OngoingActivity extends AppCompatActivity {
     String title = "", description = "";
 
     DatabaseReference databaseBooking;
+    DatabaseReference databaseGroupDetails;
+
+    //temp
+    private TextView test;
+
+    static String bookingID;
 
 
     @Override
@@ -39,9 +46,11 @@ public class OngoingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ongoing);
 
         databaseBooking = FirebaseDatabase.getInstance().getReference("bookings");
+        databaseGroupDetails = FirebaseDatabase.getInstance().getReference("groupDetails");
 
 
-        btnNextPage = (Button) findViewById(R.id.nextPage);
+
+        // btnNextPage = (Button) findViewById(R.id.nextPage);
         btnLogOut = (Button) findViewById(R.id.btnLogOut);
 
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
@@ -50,6 +59,8 @@ public class OngoingActivity extends AppCompatActivity {
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         mRecyclerView.setLayoutManager(layoutManager);
         mRecyclerView.setAdapter(mRecyclerAdapter);
+
+        test = findViewById(R.id.ongoing);
 
 
 
@@ -80,6 +91,70 @@ public class OngoingActivity extends AppCompatActivity {
         super.onStart();
 
         //pulling from firebase
+
+
+
+        databaseGroupDetails.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.hasChild("gameInputID")) {
+                    // get booking ID
+
+                    databaseGroupDetails.child("bookingID").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            bookingID = dataSnapshot.getValue().toString();
+
+                            test.setText(bookingID);
+
+
+
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+
+
+
+                    // get booking
+                    /*
+                    databaseBooking.addListenerForSingleValueEvent(new ValueEventListener() {
+
+
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            if(dataSnapshot.hasChild(bookingID)){
+
+                                DataSnapshot bookingSnapshot = (DataSnapshot) dataSnapshot.getChildren();
+                                RecyclerData bookings = bookingSnapshot.getValue(RecyclerData.class);
+
+                                myList.add(bookings);
+
+                                RecyclerAdapter2 adapter = new RecyclerAdapter2(OngoingActivity.this, myList);
+                                mRecyclerView.setAdapter(adapter);
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+                     */
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        /*
         databaseBooking.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -101,5 +176,6 @@ public class OngoingActivity extends AppCompatActivity {
 
             }
         });
+         */
     }
 }
