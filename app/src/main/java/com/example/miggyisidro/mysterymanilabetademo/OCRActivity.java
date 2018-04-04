31f4813790e8b090a9c31fe60f560e2ed80ad7b5
raffyduckk <kwatrordc@gmail.com>
2018-04-05 01:13:22 +0800

    Take and Save Photo

--- app/src/main/java/com/example/miggyisidro/mysterymanilabetademo/OCRActivity.java
+++ app/src/main/java/com/example/miggyisidro/mysterymanilabetademo/OCRActivity.java
@@ -85,83 +85,29 @@ public class OCRActivity extends AppCompatActivity {
 
         Button btnCamera = (Button) findViewById(R.id.btnCamera);
         ImageView imageView = (ImageView) findViewById(R.id.imageView);
-        final int REQUEST_TAKE_PHOTO = 1;
-        final int REQUEST_IMAGE_CAPTURE = 1;
+
 
         btnCamera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                dispatchTakePictureIntent();
-
-            }
-
-
-        });
-    }
-
-    static final int REQUEST_TAKE_PHOTO = 1;
-
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                // Error occurred while creating the File
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
-                        "com.example.android.fileprovider",
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-            }
+        @Override
+        public void onClick(View v) {
+            Intent takePhoto = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(takePhoto, 0);
 
         }
 
-    }
+
+    });
+}
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
             Bitmap bitmap = (Bitmap) data.getExtras().get("data");
             imageView.setImageBitmap(bitmap);
-            galleryAddPic();
 
-        }
     }
 
-    String mCurrentPhotoPath;
 
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-
-        // Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = image.getAbsolutePath();
-        return image;
-    }
-
-    private void galleryAddPic() {
-        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-        File f = new File(mCurrentPhotoPath);
-        Uri contentUri = Uri.fromFile(f);
-        mediaScanIntent.setData(contentUri);
-        this.sendBroadcast(mediaScanIntent);
-    }
 
 
 }
